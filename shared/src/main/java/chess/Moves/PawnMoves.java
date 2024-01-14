9f16bdbc0b88d4063f422d9c2d8d22acf4e22976
austinbeckstead <austinbeckstead314@gmail.com>
2024-01-13 19:08:52 -0700

    Added basic pawn moves

--- shared/src/main/java/chess/Moves/PawnMoves.java
+++ shared/src/main/java/chess/Moves/PawnMoves.java
@@ -1,5 +1,46 @@
 package chess.Moves;
 
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessMove;
+import chess.ChessPosition;
+import java.util.Collection;
+
 public class PawnMoves extends PieceMoves {
-    
+    private Collection<ChessMove> moves;
+    private ChessBoard board;
+    private ChessPosition myPosition;
+    private ChessGame.TeamColor teamColor;
+
+
+    public PawnMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor teamColor){
+        super(board, myPosition, teamColor);
+    }
+
+    public Collection<ChessMove> getMoves() {
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            ChessPosition forward = new ChessPosition(myPosition.getColumn(), myPosition.getRow() + 1);
+            if(myPosition.getRow() < 7) {
+                if (board.getPiece(forward) == null) {
+                    moves.add(new ChessMove(myPosition, forward, null));
+                }
+
+                if (myPosition.getColumn() > 0) {
+                    ChessPosition diag_left = new ChessPosition(myPosition.getColumn() - 1, myPosition.getRow() + 1);
+                    if (board.getPiece(diag_left) == null) {
+                        moves.add(new ChessMove(myPosition, diag_left, null));
+                    }
+                }
+                if (myPosition.getColumn() < 7) {
+                    ChessPosition diag_right = new ChessPosition(myPosition.getColumn() + 1, myPosition.getRow() + 1);
+                    if (board.getPiece(diag_right) == null) {
+                        moves.add(new ChessMove(myPosition, diag_right, null));
+                    }
+                }
+            }
+
+        }
+        return moves;
+    }
+
 }
--- shared/src/main/java/chess/Moves/PieceMoves.java
+++ shared/src/main/java/chess/Moves/PieceMoves.java
@@ -1,4 +1,5 @@
 package chess.Moves;
+
 import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPosition;
@@ -6,7 +7,7 @@ import chess.ChessPosition;
 import java.util.Collection;
 
 public class PieceMoves {
-    private Collection<ChessMove> moves = new Collection<ChessMove>();
+    private Collection<ChessMove> moves;
     private ChessBoard board;
     private ChessPosition myPosition;
     private chess.ChessGame.TeamColor teamColor;
@@ -15,5 +16,8 @@ public class PieceMoves {
         this.board = board;
         this.myPosition = myPosition;
         this.teamColor = teamColor;
+    }
+    public Collection<ChessMove> getMoves(){
+        return moves;
     }
 }
