b6f98b85c8739881fe1cd0f21ba03f93b0a219ae
austinbeckstead <austinbeckstead314@gmail.com>
2024-02-24 15:28:04 -0700

    Wrote test cases for joinGame

--- server/src/test/java/serviceTests/JoinGameTest.java
+++ server/src/test/java/serviceTests/JoinGameTest.java
@@ -1,4 +1,92 @@
 package serviceTests;
+import dataAccess.DataAccessException;
+import dataAccess.MemoryAuthDAO;
+import dataAccess.MemoryGameDAO;
+import dataAccess.MemoryUserDAO;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import passoffTests.testClasses.TestModels;
+import server.Server;
+import service.GameService;
+import service.UserService;
+import service.request.JoinRequest;
 
 public class JoinGameTest {
+    private static Server server;
+    private final MemoryGameDAO gameDAO = new MemoryGameDAO();
+    private final MemoryUserDAO userDAO = new MemoryUserDAO();
+
+    private final MemoryAuthDAO authDAO = new MemoryAuthDAO();
+    private final GameService gameService = new GameService(authDAO, userDAO, gameDAO);;
+    private final UserService userService = new UserService(authDAO, userDAO, gameDAO);;
+
+    private static TestModels.TestCreateRequest createRequest;
+    @Test
+    @Order(0)
+    @DisplayName("Join game")
+    public void joinGame() throws DataAccessException {
+        gameService.clear();
+        String username = "username";
+        userService.addUser(new UserData(username, "password", "email"));
+        AuthData token = userService.loginUser(new UserData(username, "password", null));
+        GameData result = gameService.createGame(token.authToken(), "father");
+        String message = gameService.joinGame(token.authToken(), new JoinRequest("WHITE", result.gameID()));
+        assert (message == null);
+    }
+    @Test
+    @Order(1)
+    @DisplayName("Join Full Game")
+    public void joinFullGame() throws DataAccessException {
+        gameService.clear();
+        String username = "username";
+        userService.addUser(new UserData(username, "password", "email"));
+        AuthData token = userService.loginUser(new UserData(username, "password", null));
+        GameData result = gameService.createGame(token.authToken(), "father");
+        gameService.joinGame(token.authToken(), new JoinRequest("WHITE", result.gameID()));
+        String message = gameService.joinGame(token.authToken(), new JoinRequest("WHITE", result.gameID()));
+        assert (message.equals("Error: already taken"));
+    }
+    @Test
+    @Order(2)
+    @DisplayName("Join Game no Auth")
+    public void joinNoAuth() throws DataAccessException {
+        gameService.clear();
+        String username = "username";
+        userService.addUser(new UserData(username, "password", "email"));
+        AuthData token = userService.loginUser(new UserData(username, "password", null));
+        GameData result = gameService.createGame(token.authToken(), "father");
+        String message = gameService.joinGame(null, new JoinRequest("WHITE", result.gameID()));
+        assert (message.equals("Error: unauthorized"));
+    }
+    @Test
+    @Order(3)
+    @DisplayName("Join Nonexistent Game")
+    public void joinNonexistentGame() throws DataAccessException {
+        gameService.clear();
+        String username = "username";
+        userService.addUser(new UserData(username, "password", "email"));
+        AuthData token = userService.loginUser(new UserData(username, "password", null));
+        GameData result = gameService.createGame(token.authToken(), "father");
+        String message = gameService.joinGame(token.authToken(), new JoinRequest("WHITE", 99));
+        assert (message.equals("Error: bad request"));
+    }
+    @Test
+    @Order(4)
+    @DisplayName("Spectate game")
+    public void spectateGame() throws DataAccessException {
+        gameService.clear();
+        String username = "username";
+        userService.addUser(new UserData(username, "password", "email"));
+        AuthData token = userService.loginUser(new UserData(username, "password", null));
+        GameData result = gameService.createGame(token.authToken(), "father");
+        String message = gameService.joinGame(token.authToken(), new JoinRequest(null, result.gameID()));
+        assert (message == null);
+    }
+
 }
+
+
