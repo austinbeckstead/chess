c86828d057eaf19da8480026755fdcabbb5c21ce
austinbeckstead <austinbeckstead314@gmail.com>
2024-02-24 15:43:34 -0700

    Finished clear tests

--- server/src/test/java/serviceTests/ClearTest.java
+++ server/src/test/java/serviceTests/ClearTest.java
@@ -1,8 +1,7 @@
 package serviceTests;
-import dataAccess.DataAccessException;
-import dataAccess.MemoryAuthDAO;
-import dataAccess.MemoryGameDAO;
-import dataAccess.MemoryUserDAO;
+import dataAccess.*;
+import model.AuthData;
+import model.GameData;
 import model.UserData;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
@@ -15,28 +14,50 @@ import service.UserService;
 
 public class ClearTest {
     private static Server server;
-    private final MemoryGameDAO gameDAO = new MemoryGameDAO();
-    private final MemoryUserDAO userDAO = new MemoryUserDAO();
+    private final GameDAO gameDAO = new MemoryGameDAO();
+    private final UserDAO userDAO = new MemoryUserDAO();
 
-    private final MemoryAuthDAO authDAO = new MemoryAuthDAO();
+    private final AuthDAO authDAO = new MemoryAuthDAO();
+    GameService gameService = new GameService(authDAO, userDAO, gameDAO);
+    UserService userService = new UserService(authDAO, userDAO, gameDAO);
 
     private static TestModels.TestCreateRequest createRequest;
 
     @Test
     @Order(0)
-    @DisplayName("Clear Without User")
+    @DisplayName("Clear Empty")
     public void clearTest() throws DataAccessException {
-        GameService gameService = new GameService(authDAO, userDAO, gameDAO);
         gameService.clear();
         assert gameService.isEmpty();
     }
     @Test
-    @Order(0)
+    @Order(1)
     @DisplayName("Clear With User")
     public void registerClearTest() throws DataAccessException {
-        GameService gameService = new GameService(authDAO, userDAO, gameDAO);
-        UserService userService = new UserService(authDAO, userDAO, gameDAO);
         userService.addUser(new UserData("username", "password", "email"));
+        gameService.clear();
+        assert gameService.isEmpty();
+    }
+    @Test
+    @Order(2)
+    @DisplayName("Clear Auth")
+    public void clearAuth() throws DataAccessException {
+        gameService.clear();
+        String username = "username";
+        userService.addUser(new UserData(username, "password", "email"));
+        AuthData token = userService.loginUser(new UserData(username, "password", null));
+        gameService.clear();
+        assert gameService.isEmpty();
+    }
+    @Test
+    @Order(3)
+    @DisplayName("Clear Game")
+    public void clearGame() throws DataAccessException {
+        gameService.clear();
+        String username = "username";
+        userService.addUser(new UserData(username, "password", "email"));
+        AuthData token = userService.loginUser(new UserData(username, "password", null));
+        GameData result = gameService.createGame(token.authToken(), "father");
         gameService.clear();
         assert gameService.isEmpty();
     }
