da769d8fbc038a1882549823cf202086af48c9bf
austinbeckstead <austinbeckstead314@gmail.com>
2024-03-05 14:55:54 -0700

    created clear and create auth test files

---
+++ server/src/test/java/dataAccessTests/ClearTest.java
@@ -0,0 +1,41 @@
+package dataAccessTests;
+import dataAccess.*;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import passoffTests.testClasses.TestModels;
+import server.Server;
+import service.GameService;
+import service.UserService;
+
+
+public class ClearTest {
+    private static Server server;
+    private final GameDAO gameDAO = new SqlGameDAO();
+    private final UserDAO userDAO = new SqlUserDAO();
+
+    private final AuthDAO authDAO = new SqlAuthDAO();
+
+
+    private static TestModels.TestCreateRequest createRequest;
+
+    public ClearTest() throws DataAccessException {
+    }
+    @Test
+    @Order(0)
+    @DisplayName("Clear Data")
+    public void clearData() throws DataAccessException {
+        userDAO.createUser(new UserData("username", "password", "email"));
+        AuthData token = authDAO.createAuth("username");
+        GameData result = gameDAO.createGame("gameName");
+        gameDAO.clear();
+        userDAO.clear();
+        authDAO.clear();
+        assert (gameDAO.isEmpty() && userDAO.isEmpty() && authDAO.isEmpty());
+    }
+
+}
+
---
+++ server/src/test/java/dataAccessTests/CreateAuthTest.java
@@ -0,0 +1,40 @@
+package dataAccessTests;
+import dataAccess.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import passoffTests.testClasses.TestModels;
+
+
+
+public class CreateAuthTest {
+    private final GameDAO gameDAO = new SqlGameDAO();
+    private final UserDAO userDAO = new SqlUserDAO();
+
+    private final AuthDAO authDAO = new SqlAuthDAO();
+
+
+    private static TestModels.TestCreateRequest createRequest;
+
+    public CreateAuthTest() throws DataAccessException {
+    }
+    @Test
+    @Order(0)
+    @DisplayName("Create Auth")
+    public void registerUser() throws DataAccessException {
+        assert((authDAO.createAuth("username")).username().equals("username"));
+    }
+    @Test
+    @Order(1)
+    @DisplayName("Invalid username")
+    public void createAuthTest() throws DataAccessException {
+        try {
+            authDAO.createAuth(null);
+            assert(false);
+        }catch(Exception e){
+            assert(true);
+        }
+    }
+
+}
+
